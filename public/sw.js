d94e5fbf941f5f8fe6ed7a2fe64b754afd830078
yinkaKayode26 <ykco2026@gmail.com>
2026-09-22 17:36:07 +0100

    Logo 4.6

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'naga-v5';
+const CACHE_VERSION = 'naga-v6';
 const STATIC_CACHE  = `${CACHE_VERSION}-static`;
 const DYNAMIC_CACHE = `${CACHE_VERSION}-dynamic`;
 const IMAGE_CACHE   = `${CACHE_VERSION}-images`;
